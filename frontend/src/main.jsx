7eed618def3ebb180a1f2de8df2bf5f6cc2e382d
Guru Nayak <57138569+I-c-e-m-a-n@users.noreply.github.com>
2025-11-05 17:09:05 +0800

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NewsForm from './news_email_webapp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NewsForm />
+  </React.StrictMode>
+)
