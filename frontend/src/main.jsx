ef9aee7799c17589ec3f30a13f83f57e78224a85
Guru Nayak <57138569+I-c-e-m-a-n@users.noreply.github.com>
2025-11-05 17:22:21 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import NewsForm from './news_email_webapp'
+import NewsForm from './NewsForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
