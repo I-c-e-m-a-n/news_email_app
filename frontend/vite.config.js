1ed7a567d6e8ae6f447b3cde2db92cdeb9f54393
Guru Nayak <57138569+I-c-e-m-a-n@users.noreply.github.com>
2025-11-05 17:23:43 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-
 
 export default defineConfig({
-plugins: [react()],
-base: '/news_email_app/',
-resolve: {
-alias: {
-'@': path.resolve(__dirname, './src')
-}
-}
+  plugins: [react()],
+  base: '/news_email_app/'
 })
